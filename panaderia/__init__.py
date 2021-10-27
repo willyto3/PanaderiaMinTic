c866b90cfd8077632bb208cc066cffea044ac922
Willy Corzo Lubo <82851253+willyto3@users.noreply.github.com>
2021-10-26 21:28:10 -0500

    Ya funciona de Nuevo

--- panaderia/__init__.py
+++ panaderia/__init__.py
@@ -1,7 +1,7 @@
 # import flask and sqlaclhemy
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_manager, login_required, login_user, logout_user, current_user
+
 
 app = Flask(__name__)
 
@@ -11,10 +11,6 @@ app.config["SECRET_KEY"] = 'Es un Secreto'
 app.config["UPLOAD_FOLDER"]= UPLOADE_FOLDER
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database\panaderia.db'
 db = SQLAlchemy(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
 
 from panaderia import views
 from panaderia import models
