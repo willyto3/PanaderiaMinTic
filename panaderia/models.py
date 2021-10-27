fad16c235bce9947ed3042fc06855a1065eb44de
Willy Corzo Lubo <82851253+willyto3@users.noreply.github.com>
2021-10-26 20:13:36 -0500

    Commit Inicial

---
+++ panaderia/models.py
@@ -0,0 +1,67 @@
+from sqlalchemy.orm import backref
+from panaderia import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+# MODELOS
+
+
+# Modelo para la base de datos Prueba tabla 'Productos'
+class Platos(db.Model):
+    __tablename__ = 'platos'
+    idproducto = db.Column(db.Integer, primary_key=True)
+    nombreplato = db.Column(db.String(510), nullable=True)
+    precioplato = db.Column(db.Integer, nullable=True)
+    descripcionplato = db.Column(db.String(200))
+    nombreimagenplato = db.Column(db.String(200), nullable=False)
+    
+
+# Modelo para la base de datos Prueba tabla 'Persona'
+
+
+class Personas(db.Model, UserMixin):
+    __tablename__ = 'personas'
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(200), nullable=True)
+    apellido = db.Column(db.String(200), nullable=True)
+    direccion = db.Column(db.String(200))
+    celular = db.Column(db.Integer, nullable=True)
+    email = db.Column(db.String(200), nullable=True)
+    fechanacimiento = db.Column(db.String(200))
+    #idrol = db.Column(db.Integer, db.ForeignKey("roles.idrol"))
+
+    # Genera un hash de la contraseña
+    password_hash = db.Column(db.String(200), nullable=True)
+
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "<Personas %r>" % self.nombre
+
+
+
+
+
+# Modelo para la base de datos Panaderia tabla 'Facturas'
+class Facturas(db.Model):
+    __tablename__ = 'facturas'
+    idfactura = db.Column(db.Integer, primary_key=True)
+    fechafactura = db.Column(db.Datetime, nullable=True, default =datetime.utcnow)
+
+
+# Modelo para la base de datos Panaderia tabla 'Roles'
+class Roles(db.Model):
+    __tablename__ = 'roles'
+    idrol = db.Column(db.Integer, primary_key=True)
+    rol = db.Column(db.String(10), unique=True, nullable=True)
+    #personas = db.relationship("Persona", backref="Roles")
---
+++ panaderia/views.py
@@ -0,0 +1,142 @@
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from werkzeug.utils import secure_filename
+from panaderia.models import *
+from panaderia import app
+import os
+
+# Creacion de las Paginas
+
+# Pagina Principal
+@app.route('/')
+def index():
+    platos=Platos.query.all()
+    return render_template('/index.html', platos=platos)
+
+# Pagina de Inicio de Sesion
+@app.route('/Login')
+def login():
+    return render_template('login.html')
+
+# Pagina de Registro
+@app.route('/Registro')
+def registro():
+    return render_template('registro.html')
+
+# Pagina de Creacion de Platos
+@app.route('/CrearPlatos')
+def crearplatos():
+    return render_template('crearplatos.html')
+
+
+@app.route('/Busqueda')
+def busqueda():
+    return render_template('busqueda.html')
+
+
+@app.route('/Menu')
+def menu():
+    platos=Platos.query.all()
+    return render_template('menu.html', platos=platos)
+
+
+@app.route('/Perfil')
+def perfil():
+    return render_template('perfil.html')
+
+
+@app.route('/Carrito')
+def carrito():
+    return render_template('carrito.html')
+
+
+@app.route('/Dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/Comentarios')
+def comentarios():
+    return render_template('comentarios.html')
+
+
+@app.route('/Usuarios')
+def usuarios():
+    return render_template('usuarios.html')
+
+
+def pagina_no_encontrada(error):
+    return redirect(url_for('index'))
+
+
+def error_de_servidor(error):
+    return redirect(url_for('index'))
+
+# FUNCIONES
+
+# Funcion Crear Registro
+
+
+@app.route('/crearregistro', methods=['POST'])
+def create():
+
+    if request.form['password_hash'] == request.form['password_hash2']:
+        try:
+            pw_hash = generate_password_hash(
+                request.form['password_hash'], "sha256")
+            persona = Personas(nombre=request.form['nombre'], apellido=request.form['apellido'],
+                               direccion=request.form['direccion'], celular=request.form['celular'],
+                               email=request.form['email'], fechanacimiento=request.form['fechanacimiento'],
+                               password_hash=pw_hash)
+            db.session.add(persona)
+            db.session.commit()
+            flash('Registro creado exitosamente')
+            return render_template('registro.html')
+
+        except Exception as e:
+            flash("No se realizo el registro del Usuario")
+            return render_template('registro.html')
+    else:
+        flash("Las Contraseñas deben ser iguales")
+        return render_template('registro.html')
+
+
+# Funcion Ingresar
+@app.route('/ingresar', methods=['POST'])
+def ingreso():
+
+    try:
+        persona = Personas.query.filter_by(
+            celular=request.form['celular']).first()
+        if persona and check_password_hash(persona.password_hash, request.form['contrasena']):
+            return render_template('perfil.html')
+        else:
+            flash('Usuario o Contraseña incorrectos')
+            return render_template('login.html')
+    except Exception as e:
+        flash('Usuario o Contraseña incorrectos')
+        return render_template('login.html')
+
+
+    # Funcion Crear Plato
+
+@app.route('/crearplatos', methods=['POST'])
+def crearplato():
+
+    imagen = request.files['imagenplato']
+
+    filename = secure_filename(imagen.filename)
+    imagen.save(os.path.join(app.config["UPLOAD_FOLDER"],filename))
+    
+    plato = Platos(nombreplato=request.form['nombreplato'], precioplato=request.form['precioplato'], descripcionplato=request.form['descripcionplato'], nombreimagenplato=filename)
+    db.session.add(plato)
+    db.session.commit()
+    flash('Registro creado exitosamente')
+    return render_template('crearplatos.html')
+
+# CLASES DE FORMULARIO
+
+
